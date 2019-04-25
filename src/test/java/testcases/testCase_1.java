a4a6f0ce89c361a666a36e64d3e0ce72b44043d0
sarangholey <sarangholey@gmail.com>
2019-04-25 15:34:48 +0530

    Initial commit

---
+++ src/test/java/testcases/testCase_1.java
@@ -0,0 +1,27 @@
+package testcases;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class testCase_1 
+{
+
+	@Test
+	public void rightClickConsecept() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get("http://demo.guru99.com/test/simple_context_menu.html");
+		System.out.println(driver.getTitle());
+		
+	}
+	
+}
