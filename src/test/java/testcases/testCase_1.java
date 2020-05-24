ac9f8c5f5245c833f9545b3e77d31127c51a38f9
sarangholey <sarangholey@gmail.com>
2020-05-24 21:23:37 +0530

    given priority to the test cases

--- src/test/java/testcases/testCase_1.java
+++ src/test/java/testcases/testCase_1.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 public class testCase_1 
 {
 
-	@Test
+	@Test(priority = 1)
 	public void rightClickConsecept() throws InterruptedException
 	{
 		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
@@ -22,11 +22,11 @@ public class testCase_1
 		driver.get("http://demo.guru99.com/test/simple_context_menu.html");
 		System.out.println("Title of entered Site is "+driver.getTitle());
 		
-		// test
+		driver.quit();
 		
 	}
 	
-	@Test
+	@Test(priority = 2)
 	public void rightClickConsecept1() throws InterruptedException
 	{
 		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
@@ -38,9 +38,10 @@ public class testCase_1
 		
 		driver.findElement(By.name("q")).sendKeys("Test");
 		
+		driver.quit();
 	}
 	
-	@Test
+	@Test(priority = 3)
 	public void rightClickConsecept2() throws InterruptedException
 	{
 		System.setProperty("webdriver.chrome.driver","chromedriver.exe");
@@ -51,6 +52,8 @@ public class testCase_1
 		System.out.println("Title of entered Site is "+driver.getTitle());
 		
 		driver.findElement(By.name("q")).sendKeys("Selenium");
+		
+		driver.quit();
 		
 	}
 }
